6c780ff99b8c7baa8c065a8700777ea3ef778581
belmoz <sosnin.serg@gmail.com>
2022-11-23 13:31:12 +0200

    added middleware save to localStorage

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -14,6 +14,5 @@ export const store = configureStore({
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(localStorageMiddleware.middleware)
 })
 
-
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
